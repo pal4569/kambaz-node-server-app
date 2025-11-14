7a86cb312e443d9c9fb1f9773da813672bd0c214
pal4569 <michaelfc8841@gmail.com>
2025-11-14 17:35:35 -0500

    added dependencies

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -28,6 +28,7 @@ export default function CoursesDao(db) {
   function updateCourse(courseId, courseUpdates) {
     const { courses } = db;
     const course = courses.find((course) => course._id === courseId);
+    console.log(course);
     Object.assign(course, courseUpdates);
     return course;
   }
